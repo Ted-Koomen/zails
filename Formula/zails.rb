023bee2308530194312c0ec2a7d76483f7d92014
Ted Koomen <koomen.ted@gmail.com>
2026-03-06 14:39:34 -0500

    Update zails.rb

--- Formula/zails.rb
+++ Formula/zails.rb
@@ -1,18 +1,18 @@
 class Zails < Formula
   desc "High-performance gRPC Rails-like framework for Zig"
-  homepage "https://github.com/ted-koomen/zails"
+  homepage "https://github.com/tedkoomen/zails"
   version "0.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ted-koomen/zails/releases/download/v#{version}/zails-aarch64-macos.tar.gz"
+      url "https://github.com/tedkoomen/zails/releases/download/v#{version}/zails-aarch64-macos.tar.gz"
       sha256 "PLACEHOLDER"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ted-koomen/zails/releases/download/v#{version}/zails-x86_64-linux.tar.gz"
+      url "https://github.com/tedkoomen/zails/releases/download/v#{version}/zails-x86_64-linux.tar.gz"
       sha256 "PLACEHOLDER"
     end
   end
